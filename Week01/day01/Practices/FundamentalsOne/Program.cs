c9552afec8dc0a1dd2a3edb66ca1ed152abba2a2
AliBourara <bourara.ali.pro@gmail.com>
2023-10-02 19:44:06 +0200

    Created FandamentalsOne Practice assaignement

---
+++ Week01/day01/Practices/FundamentalsOne/Program.cs
@@ -0,0 +1,17 @@
+﻿int i = 1;
+while(i <=255)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        System.Console.WriteLine($"FizzBizz");
+    }
+    else if (i % 5 == 0)
+    {
+        System.Console.WriteLine($"Bizz");
+    }
+    else if (i % 3 == 0)
+    {
+        System.Console.WriteLine($"Fizz");
+    }
+    i++;
+}
